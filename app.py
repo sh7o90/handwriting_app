79ae66ff265181d3cff62129ce22efd96f47e6e1
sh7o90 <sh7.games@gmail.com>
2024-05-14 14:44:23 +0900

    READMEの追加

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 # 必要なライブラリをインポート
 import streamlit as st  # Streamlitライブラリ
 from streamlit_drawable_canvas import st_canvas  # 描画キャンバス用ライブラリ
-from PIL import Image  # 画像処理用ライブラリ
+from PIL import Image, ImageColor  # 画像処理用ライブラリ
 import torch  # PyTorchライブラリ
 import torchvision.transforms as transforms  # 画像変換処理用ライブラリ
 import torch.nn as nn  # PyTorchのニューラルネットワークライブラリ
 import timm  # PyTorch Image Models (timm)ライブラリ
 import pickle  # ピクルスファイル読み込み用ライブラリ
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+import numpy as np
 
 # 画像をテンソルに変換するための変換パイプライン
 transform = transforms.Compose([
@@ -26,6 +29,14 @@ labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] + \
 def get_label(class_id):
     return labels[class_id]
 
+# 画像を前処理する関数
+def preprocess_image(image):
+    image_gray = image[:, :, 3]
+    image_pil = Image.fromarray(image_gray)
+    image_resize = image_pil.resize((28, 28))
+    image_tensor = transform(image_resize).unsqueeze(0)
+    return image_tensor
+
 # EfficientNetV2_MNISTモデルの定義
 class EfficientNetV2_EMNIST(nn.Module):
     def __init__(self, num_classes=47):
@@ -41,22 +52,28 @@ class EfficientNetV2_EMNIST(nn.Module):
         return self.efficientnet(x)
 
 # Streamlitアプリのタイトル
-st.title("EMNIST Character Recognition")
+st.title("手書き文字認識アプリ")
+
+# 白色の背景画像を作成
+width, height = 300, 300  # キャンバスのサイズに合わせる
+background_image = Image.new('RGB', (width, height), color=ImageColor.getrgb('white'))
 
 # レイアウトの調整
 col1, col2 = st.columns([2, 1])  # キャンバスと予測ボタンの列を作成
 
 # Canvas領域の作成
 with col1:
-    st.subheader("Draw a Digit")
+    st.subheader("文字を書いてください")
     canvas_result = st_canvas(
         stroke_width=20,
         stroke_color="#000",
+        background_image=background_image,
         update_streamlit=True,
         height=300,
         width=300,
         drawing_mode="freedraw",
         key="canvas",
+        display_toolbar=True,  # ツールバーを表示
     )
 
 # モデルのロード
@@ -65,33 +82,46 @@ with open('last_efficientnetv2_emnist.pkl', 'rb') as f:
     net.load_state_dict(pickle.load(f))  # 学習済みモデルの読み込み
 net.eval()  # 推論モードに設定
 
-# 画像を前処理する関数
-def preprocess_image(image):
-    # アルファチャンネルだけ取得してグレースケールに変換
-    image_gray = image[:, :, 3]
-    # Numpy配列をPIL Imageに変換
-    image_pil = Image.fromarray(image_gray)
-    # 28x28ピクセルにリサイズ
-    image_resize = image_pil.resize((28, 28))
-    # 変換を適用してテンソルに変換
-    image_tensor = transform(image_resize).unsqueeze(0)
-    return image_tensor
-
 # 予測ボタンが押されたときの処理
 with col2:
-    st.subheader("Prediction")
+    st.subheader("予測結果")
     predict_button = st.button("Predict", key="predict_button")
     if predict_button:
-        with st.spinner("Predicting..."):  # ローディングアニメーションを追加
+        with st.spinner("予測中..."):  # ローディングアニメーションを追加
             # 描画された画像データを取得し、RGBA形式からRGB形式に変換
             image_data = canvas_result.image_data
             # 画像を前処理
             processed_image = preprocess_image(image_data)
+
             # 予測
             with torch.no_grad():
                 outputs = net(processed_image)
-                # 最も確率の高いクラスのインデックスを取得
-                predicted_class = torch.argmax(outputs, dim=1).item()
 
-            # 予測結果を表示
-            st.success(f"Predicted Digit: {predicted_class}({get_label(predicted_class)})")
+            probabilities = torch.softmax(outputs, dim=1)
+
+            # 確率が高い順に並び替え
+            sorted_probs, sorted_indices = torch.sort(probabilities, dim=1, descending=True)
+
+            # 予測結果を大きい文字で表示
+            result_text = f"結果: {get_label(sorted_indices[0, 0].item())}"
+            st.markdown(f"<h1 style='text-align: center; color: Text color;'>{result_text}</h1>", unsafe_allow_html=True)
+
+            # 上位5つのクラスとその確率を取得
+            labels = [get_label(idx) for idx in sorted_indices[0, :5]]
+            values = [sorted_probs[0, i].item() for i in range(5)]
+
+with st.expander("クリックして展開"):
+    # サブヘッダーを中央揃えで表示
+    st.markdown(f"<h3 style='text-align: center; color: Text color;'>確率分布</h3>", unsafe_allow_html=True)
+    if predict_button:
+        # 棒グラフを描画
+        fig, ax = plt.subplots(figsize=(8, 6))  # グラフサイズを大きくする
+        ax.bar(labels, values, width=0.6)  # 棒の幅を調整
+        ax.set_xlabel("クラス", fontsize=14)  # ラベルのフォントサイズを大きくする
+        ax.set_ylabel("確率", fontsize=14)
+        ax.tick_params(axis='x', labelsize=12)  # x軸ラベルの角度と文字サイズを調整
+        ax.grid(True)  # グリッドを表示
+        ax.set_axisbelow(True)  # グリッドを背面に表示
+
+        # Streamlitで棒グラフを表示
+        st.pyplot(fig)
